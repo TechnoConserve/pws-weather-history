384e16b40b309eaf4ccd71c0e6c1a4f99d999e62
Avery Uslaner <uslaner.avery@gmail.com>
2016-10-28 15:41:31 -0600

    Finished parsing daily data and downloading historical data to csv.

---
+++ weather-parse.py
@@ -0,0 +1,132 @@
+from datetime import date
+import xml.etree.ElementTree as ET
+
+import openpyxl
+import pandas as pd
+
+
+def grab_history():
+	day, month, year = date.today().strftime('%d %m %Y').split(' ')
+	url = 'https://www.wunderground.com/weatherstation/WXDailyHistory.asp?' \
+		'ID=KAZLITTL3&day=13&month=10&year=2016&' \
+		'dayend=' + day + '&monthend=' + month + '&yearend=' + year + \
+		'&graphspan=custom&format=1'
+	df = pd.read_csv(url, header=0, index_col=0)
+	df.rename(columns = {'PrecipitationSumIn<br>': 'Precipitation Sum (in)'}, inplace=True)
+	df = df[df.index != '<br>']
+	df.to_csv('updated-' + day + '-' + month + '-' + year +'blackrock-weather.csv')
+
+
+def parse_today():
+	tree = ET.parse('WXDailyHistory.xml')
+	root = tree.getroot()
+	
+	wb = openpyxl.Workbook()
+	sheet = wb.active
+
+	sheet['A1'] = 'Observation Time'
+	sheet['B1'] = 'Location Full'
+	sheet['C1'] = 'Neighborhood'
+	sheet['D1'] = 'City'
+	sheet['E1'] = 'State'
+	sheet['F1'] = 'Zip'
+	sheet['G1'] = 'Latitude'
+	sheet['H1'] = 'Longitude'
+	sheet['I1'] = 'Elevation'
+	sheet['J1'] = 'Temp (f)'
+	sheet['K1'] = 'Temp (c)'
+	sheet['L1'] = 'Relative Humidity'
+	sheet['M1'] = 'Wind'
+	sheet['N1'] = 'Wind Direction'
+	sheet['O1'] = 'Wind Degrees'
+	sheet['P1'] = 'Wind (mph)'
+	sheet['Q1'] = 'Wind Gust (mph)'
+	sheet['R1'] = 'Pressure'
+	sheet['S1'] = 'Pressure (mb)'
+	sheet['T1'] = 'Pressure (in)'
+	sheet['U1'] = 'Dewpoint'
+	sheet['V1'] = 'Dewpoint (f)'
+	sheet['W1'] = 'Dewpoint (c)'
+	sheet['X1'] = 'Solar Radiation'
+	sheet['Y1'] = 'UV'
+	sheet['Z1'] = 'Precipitation Last Hour'
+	sheet['AA1'] = 'Precipitation Last Hour (in)'
+	sheet['AB1'] = 'Precipitation Last Hour (mm)'
+	sheet['AC1'] = 'Precipitation Today'
+	sheet['AD1'] = 'Precipitation Today (in)'
+	sheet['AE1'] = 'Precipitation Today (mm)'
+
+	for idx, observation in enumerate(root):
+		observation_time = observation.find('observation_time').text
+
+		location = observation.find('location')
+		location_full = location[0].text
+		location_neighborhood = location[1].text
+		location_city = location[2].text
+		location_state = location[3].text
+		location_zip = location[4].text
+		location_latitude = location[5].text
+		location_longitude = location[6].text
+		location_elevation = location[7].text
+
+		temp_f = observation.find('temp_f').text
+		temp_c = observation.find('temp_c').text
+		relative_humidity = observation.find('relative_humidity').text
+		wind = observation.find('wind_string').text
+		wind_dir = observation.find('wind_dir').text
+		wind_degrees = observation.find('wind_degrees').text
+		wind_mph = observation.find('wind_mph').text
+		wind_gust_mph = observation.find('wind_gust_mph').text
+		pressure_string = observation.find('pressure_string').text
+		pressure_mb = observation.find('pressure_mb').text
+		pressure_in = observation.find('pressure_in').text
+		dewpoint_string = observation.find('dewpoint_string').text
+		dewpoint_f = observation.find('dewpoint_f').text
+		dewpoint_c = observation.find('dewpoint_c').text
+		solar_radiation = observation.find('solar_radiation').text
+		UV = observation.find('UV').text
+		precip_1hr_string = observation.find('precip_1hr_string').text
+		precip_1hr_in = observation.find('precip_1hr_in').text
+		precip_1hr_metric = observation.find('precip_1hr_metric').text
+		precip_today_string = observation.find('precip_today_string').text
+		precip_today_in = observation.find('precip_today_in').text
+		precip_today_metric = observation.find('precip_today_metric').text
+
+		sheet['A' + str(idx + 2)] = observation_time
+		sheet['B' + str(idx + 2)] = location_full
+		sheet['C' + str(idx + 2)] = location_neighborhood
+		sheet['D' + str(idx + 2)] = location_city
+		sheet['E' + str(idx + 2)] = location_state
+		sheet['F' + str(idx + 2)] = location_zip
+		sheet['G' + str(idx + 2)] = location_latitude
+		sheet['H' + str(idx + 2)] = location_longitude
+		sheet['I' + str(idx + 2)] = location_elevation
+		sheet['J' + str(idx + 2)] = temp_f
+		sheet['K' + str(idx + 2)] = temp_c
+		sheet['L' + str(idx + 2)] = relative_humidity
+		sheet['M' + str(idx + 2)] = wind
+		sheet['N' + str(idx + 2)] = wind_dir
+		sheet['O' + str(idx + 2)] = wind_degrees
+		sheet['P' + str(idx + 2)] = wind_mph
+		sheet['Q' + str(idx + 2)] = wind_gust_mph
+		sheet['R' + str(idx + 2)] = pressure_string
+		sheet['S' + str(idx + 2)] = pressure_mb
+		sheet['T' + str(idx + 2)] = pressure_in
+		sheet['U' + str(idx + 2)] = dewpoint_string
+		sheet['V' + str(idx + 2)] = dewpoint_f
+		sheet['W' + str(idx + 2)] = dewpoint_c
+		sheet['X' + str(idx + 2)] = solar_radiation
+		sheet['Y' + str(idx + 2)] = UV
+		sheet['Z' + str(idx + 2)] = precip_1hr_string
+		sheet['AA' + str(idx + 2)] = precip_1hr_in
+		sheet['AB' + str(idx + 2)] = precip_1hr_metric
+		sheet['AC' + str(idx + 2)] = precip_today_string
+		sheet['AD' + str(idx + 2)] = precip_today_in
+		sheet['AE' + str(idx + 2)] = precip_today_metric
+
+	todays_date = date.today().strftime('%d-%m-%Y')
+	wb.save(todays_date + '-blackrock-weather.xlsx')
+	
+
+if __name__ == '__main__':
+	grab_history()	
