3329d4d8a4bec3cc98f44067b7f8f8744ecd2b2a
Avery Uslaner <uslaner.avery@gmail.com>
2017-11-01 15:24:02 -0600

    Added title to application window.

--- weather-parse.py
+++ weather-parse.py
@@ -3,22 +3,23 @@ import xml.etree.ElementTree as ET
 
 import openpyxl
 import pandas as pd
-import tkinter as tk
+from tkinter import *
+from tkinter import ttk
 
 
-class Application(tk.Frame):
+class Application(Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.pack()
         self.create_widgets()
 
     def create_widgets(self):
-        self.hi_there = tk.Button(self)
+        self.hi_there = Button(self)
         self.hi_there['text'] = 'Hello World\n(click me)'
         self.hi_there['command'] = self.say_hi
         self.hi_there.pack(side='top')
 
-        self.quit = tk.Button(self, text='QUIT', fg='red', command=root.destroy)
+        self.quit = Button(self, text='QUIT', fg='red', command=root.destroy)
 
         self.quit.pack(side='bottom')
 
@@ -152,6 +153,7 @@ def parse_today():
 if __name__ == '__main__':
     #grab_history()
 
-    root = tk.Tk()
+    root = Tk()
+    root.title("Get Weather History")
     app = Application(master=root)
     app.mainloop()
